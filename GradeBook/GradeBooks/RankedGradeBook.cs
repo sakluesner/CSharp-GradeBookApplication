d714692244707478fee7c046d076cb4f326bb560
sakluesner <65417445+sakluesner@users.noreply.github.com>
2020-05-15 13:43:10 -0500

    Creating New GradeBook Types

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook: BaseGradeBook
+    {
+        public RankedGradeBook(string name): base(name)
+        {
+            var gradeBook = new BaseGradeBook(name);
+            gradeBook.Type = GradeBookType.Ranked;
+        }
+    }
+}

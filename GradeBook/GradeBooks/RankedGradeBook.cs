0588310854402023fb25e18cb6147fc81f215389
sakluesner <65417445+sakluesner@users.noreply.github.com>
2020-05-15 14:59:49 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -18,29 +18,38 @@ namespace GradeBook.GradeBooks
 
             var grade = new Char();
 
-           if (averageGrade >= 80)
-            {
-                grade = 'A';
-            }
-           else if (averageGrade >= 60)
-            {
-                grade = 'B';
-            }
-           else if (averageGrade >= 40)
-            {
-                grade = 'C';
-            }
-           else if (averageGrade >= 20)
-            {
-                grade = 'D';
-            }
-           else
-            {
-                grade = 'F';
-            }
-                
-                           
+           if (averageGrade >= 80) grade = 'A';
+           else if (averageGrade >= 60) grade = 'B';
+           else if (averageGrade >= 40) grade = 'C';
+           else if (averageGrade >= 20) grade = 'D';
+           else grade = 'F';
+
             return grade;
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (this.Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (this.Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+
         }
     }
 }
